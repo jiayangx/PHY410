e58d87efcec130109271023484728217b7486108
jiayangx <jiayangx@buffalo.edu>
2017-02-20 16:39:13 -0500

    Adding promblem1.cc

--- Homework2/problem1.cc
+++ Homework2/problem1.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <limits>
 
 int main(){
   
@@ -7,16 +9,19 @@ int main(){
   std::cin >> x1 >> y1 >> x2 >> y2;
   midpointx=(x1+x2)/2;
   midpointy=(y1+y2)/2;
-  if (x2==x1){
+  float tolerance =0.001f;
+  if (std::abs(x1-x2) < tolerance){
     std::cout << "Slope is undefined" <<'\n';
     std::cout << "y intercept is y-axis" <<'\n';
     std::cout << "The midpoint is: (" << midpointx << "," << midpointy << ")"<<'\n';
     std::cout << "And the line equation: x=" << x1 <<'\n'; }
-  else {slope=(y2-y1)/(x2-x1);
+  else if (std::abs(y1-y2) < tolerance) {slope=0;}
+  else {slope=(y2-y1)/(x2-x1);}
   yintercept=y1-slope*x1;
   std::cout << "The midpoint is: (" << midpointx << "," << midpointy << ")"<< '\n';
+  std::cout << "The yintercept is: " << yintercept << '\n';
   std::cout << "The slope is: " << slope << '\n';
-  std::cout << "And the line equation: y=" << slope << "x+" << yintercept << '\n';}
+  std::cout << "And the line equation: y = (" << slope << ") x + (" << yintercept << ")" << '\n';
 
   return 0;
 }
